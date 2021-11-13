06af37a845ba0ff4fbd3342caa99070855e9c488
edoomm <edoomm8@gmail.com>
2021-11-13 16:58:59 -0600

    se inicia con la GUI para administrar las propuestas sin ID

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,7 +5,7 @@ const router = express.Router();
 const controladorUsuarios = require('../controllers/controlador-usuarios');
 
 // rutas
-router.get('/propuestas', async (req, res, next) => {
+router.get('/propuestas', async (req, res) => {
     const idUsuario = req.session.passport.user;
     const usuario = await controladorUsuarios.obtenerUsuario(idUsuario);
     res.render('./admin/propuestas_tesis', {tipoUsuario: usuario.type});
