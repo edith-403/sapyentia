aaf24b2cf1bcfc728f8e7f17fde13956161b9586
unknown <ayala.segoviano.donaldo@gmail.com>
2021-11-22 21:47:48 -0600

    Se dio formato al documento con prettier y se añadió nueva ruta para consultar tesis por id

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,114 +1,143 @@
-const router = require('express').Router();
-const passport = require('passport');
-const multer = require('multer');
-const controladorRegistros = require('../controllers/controlador-registro-tesis')
-const controladorConsultasTesis = require('../controllers/controlador-peticion-tesis');
-const controladorConsultaHistorial = require('../controllers/controlador-peticion-historial');
-const controladorUsuarios = require('../controllers/controlador-usuarios');
-const uuid = require('uuid');
+const router = require("express").Router();
+const passport = require("passport");
+const multer = require("multer");
+const controladorRegistros = require("../controllers/controlador-registro-tesis");
+const controladorConsultasTesis = require("../controllers/controlador-peticion-tesis");
+const controladorConsultaHistorial = require("../controllers/controlador-peticion-historial");
+const controladorUsuarios = require("../controllers/controlador-usuarios");
+const uuid = require("uuid");
 
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get("/", (req, res, next) => {
+  res.render("index");
 });
 
-router.post('/tesis', async (req, res, next) => {
-  const resultados = await controladorConsultasTesis.procesarSolicitudTesis(req.body);
+router.post("/tesis", async (req, res, next) => {
+  const resultados = await controladorConsultasTesis.procesarSolicitudTesis(
+    req.body
+  );
   res.send(resultados);
 });
 
-router.get('/view/tesis', async (req, res, next) => {
-  res.render('consultas_tesis');
+router.get("/tesis/:id", async (req, res, next) => {
+  console.log(controladorConsultasTesis)
+  const result = await controladorConsultasTesis.obtenerInformacionTesis({
+    numero: req.params.id,
+  });
+  res.send(result[0]);
 });
 
-router.get('/signup', (req, res, next) => {
-  res.render('signup');
+router.get("/tesis", async (req, res, next) => {
+  const resultados = await controladorConsultasTesis.procesarSolicitudTesis(
+    req.body
+  );
+  res.send(resultados);
 });
 
-router.post('/signup', passport.authenticate('local-signup', {
-  successRedirect: '/profile',
-  failureRedirect: '/signup',
-  failureFlash: true
-})); 
-
-router.get('/signin', (req, res, next) => {
-  res.render('signin');
+router.get("/view/tesis", async (req, res, next) => {
+  res.render("consultas_tesis");
 });
 
-router.post('/signin', passport.authenticate('local-signin', {
-  successRedirect: '/profile',
-  failureRedirect: '/signin',
-  failureFlash: true
-}));
-
-router.get('/tesis', async (req, res, next) => {
-  const results = await controladorConsultasTesis.procesarSolicitudTesis(req.body);
-  res.send(results);
+router.get("/signup", (req, res, next) => {
+  res.render("signup");
 });
+
+router.post(
+  "/signup",
+  passport.authenticate("local-signup", {
+    successRedirect: "/profile",
+    failureRedirect: "/signup",
+    failureFlash: true,
+  })
+);
+
+router.get("/signin", (req, res, next) => {
+  res.render("signin");
+});
+
+router.post(
+  "/signin",
+  passport.authenticate("local-signin", {
+    successRedirect: "/profile",
+    failureRedirect: "/signin",
+    failureFlash: true,
+  })
+);
 
 const storage = multer.diskStorage({
-  destination: './propuestas',
+  destination: "./propuestas",
   filename: function (req, file, done) {
-    done('', file.originalname)
-  }
+    done("", file.originalname);
+  },
 });
 
 const upload = multer({
-  storage: storage
+  storage: storage,
 });
 
-router.get('/propuestas', (req, res, next) => {
-  res.render('formulario_propuesta', {success: '', status: ''});
+router.get("/propuestas", (req, res, next) => {
+  res.render("formulario_propuesta", { success: "", status: "" });
 });
 
-router.post('/propuestas', upload.single('archivo'), async (req, res, next) => {
-  const result = await controladorRegistros.registrarPropuesta(req.body, req.file.originalname);
+router.post("/propuestas", upload.single("archivo"), async (req, res, next) => {
+  const result = await controladorRegistros.registrarPropuesta(
+    req.body,
+    req.file.originalname
+  );
   if (result === false) {
-    res.render('formulario_propuesta', {success: 'La tesis ya existe', status: 'danger'});
-  }
-  else {
-    res.render('formulario_propuesta', {success: 'Propuesta enviada correctamente', status: 'success'});
+    res.render("formulario_propuesta", {
+      success: "La tesis ya existe",
+      status: "danger",
+    });
+  } else {
+    res.render("formulario_propuesta", {
+      success: "Propuesta enviada correctamente",
+      status: "success",
+    });
   }
 });
 
 router.use((req, res, next) => {
-    isAuthenticated(req, res, next);
+  isAuthenticated(req, res, next);
 });
 
-router.get('/profile', async (req, res, next) => {
+router.get("/profile", async (req, res, next) => {
   const idUsuario = req.session.passport.user;
   const usuario = await controladorUsuarios.obtenerUsuario(idUsuario);
-  res.render('profile', {tipoUsuario: usuario.type});
+  res.render("profile", { tipoUsuario: usuario.type });
 });
 
-router.get('/docente/historial/:id', async (req, res, next) => {
-  const result = await controladorConsultaHistorial.buscarTesisProfesor({numero: req.params.id});
+router.get("/docente/historial/:id", async (req, res, next) => {
+  const result = await controladorConsultaHistorial.buscarTesisProfesor({
+    numero: req.params.id,
+  });
   res.send(result[0]);
 });
 
-router.get('/docente/historial', async (req, res, next) => {
+router.get("/docente/historial", async (req, res, next) => {
   const idUsuario = req.session.passport.user;
   const usuario = await controladorUsuarios.obtenerUsuario(idUsuario);
   if (usuario.type === "docente") {
-    const tesisRelacionadas = await controladorConsultaHistorial.buscarTesisProfesor({
-      "directores": usuario.email,
-      "sinodales": usuario.email
-    });
-    res.render('tabla_historial', {tesis: tesisRelacionadas});
+    const tesisRelacionadas =
+      await controladorConsultaHistorial.buscarTesisProfesor({
+        directores: usuario.email,
+        sinodales: usuario.email,
+      });
+    res.render("tabla_historial", { tesis: tesisRelacionadas });
   } else {
-    res.redirect('/profile');
+    res.redirect("/profile");
   }
 });
 
-router.get('/logout', (req, res, next) => {
+router.get("/logout", (req, res, next) => {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
 function isAuthenticated(req, res, next) {
-  if(req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
     return next();
   }
-  res.redirect('/')
+  res.redirect("/");
 }
 
 module.exports = router;
