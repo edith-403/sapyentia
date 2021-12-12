4bfd1be325459b190372a3e6547923f470c8f06f
edoomm <edoomm8@gmail.com>
2021-12-11 22:38:27 -0600

    home del administrador pasado a entorno final
    TODO: Al iniciar sesion mandar a /admin

--- src/routes/tests.js
+++ src/routes/tests.js
@@ -1,22 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-// controladores
-const controladorUsuarios = require('../controllers/controlador-usuarios');
-const controladorConsultasTesis = require("../controllers/controlador-peticion-tesis");
+// // controladores
+// const controladorUsuarios = require('../controllers/controlador-usuarios');
+// const controladorConsultasTesis = require("../controllers/controlador-peticion-tesis");
 
 // rutas
 
-router.get('/', async (req, res) => {
-    const usuario = await controladorUsuarios.obtenerUsuario("618eb2881f1522f958590f34");
+// router.get('/', async (req, res) => {
+//     const usuario = await controladorUsuarios.obtenerUsuario("618eb2881f1522f958590f34");
     
-    if (usuario.type === "administrador") {
-        const info = await controladorConsultasTesis.obtenerNumeroTesis();
+//     if (usuario.type === "administrador") {
+//         const info = await controladorConsultasTesis.obtenerNumeroTesis();
 
-        res.render('./tests/home/admin', {tipoUsuario: usuario.type, nombre: usuario.nombre, email: usuario.email, numeroPropuestas: info.numeroPropuestas, numeroTesis: info.numeroTesis});
-    } else {
-        res.send('Error');
-    }
-});
+//         res.render('./tests/home/admin', {tipoUsuario: usuario.type, nombre: usuario.nombre, email: usuario.email, numeroPropuestas: info.numeroPropuestas, numeroTesis: info.numeroTesis});
+//     } else {
+//         res.send('Error');
+//     }
+// });
 
 module.exports = router;
